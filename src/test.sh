b45b1e0174ffb7d695859380efc05f0b1eea6469
wh-xu <xuweih0712@gmail.com>
2021-05-17 20:30:36 +0800

    Add Correlated Predictor

---
+++ src/test.sh
@@ -0,0 +1,14 @@
+make clean
+make
+
+
+SCHEME="gshare"
+PARMS=5
+
+# Testing all traces
+echo "\n\nBranch prediction scheme: $SCHEME with parms: $PARMS"
+for i in "fp_1.bz2" "fp_2.bz2" "int_1.bz2" "int_2.bz2" "mm_1.bz2" "mm_2.bz2"; 
+    do echo "\n$i"; 
+    eval "bunzip2 -kc ../traces/$i | ./predictor --$SCHEME:$PARMS"
+done
+
