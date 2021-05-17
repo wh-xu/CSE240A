a19a28340b28a9ba7f20a26de39ebcc8524b21c5
wh-xu <xuweih0712@gmail.com>
2021-05-17 20:30:58 +0800

    Add Correlated Predictor

--- src/predictor.c
+++ src/predictor.c
@@ -11,17 +11,17 @@
 //
 // TODO:Student Information
 //
-const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentName = "Weihong Xu";
+const char *studentID = "A59002183";
+const char *email = "wexu@ucsd.edu";
 
 //------------------------------------//
 //      Predictor Configuration       //
 //------------------------------------//
 
 // Handy Global for use in output routines
-const char *bpName[4] = { "Static", "Gshare",
-                          "Tournament", "Custom" };
+const char *bpName[4] = {"Static", "Gshare",
+                         "Tournament", "Custom"};
 
 int ghistoryBits; // Number of bits used for Global History
 int lhistoryBits; // Number of bits used for Local History
@@ -37,6 +37,15 @@ int verbose;
 //TODO: Add your own Branch Predictor data structures here
 //
 
+#define GSHARE_MAX_BIT 10
+
+struct gshare_global
+{
+  uint8_t pattern[1 << GSHARE_MAX_BIT];
+  uint8_t state[GSHARE_MAX_BIT];
+  uint8_t bit;
+  int last_pred_index;
+} gshare_global_predictor;
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -44,12 +53,19 @@ int verbose;
 
 // Initialize the predictor
 //
-void
-init_predictor()
+void init_predictor()
 {
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+  // 1. Initialize GSHARE-based predictor
+  gshare_global_predictor.bit = ghistoryBits;
+  memset(gshare_global_predictor.pattern, 0, (1 >> GSHARE_MAX_BIT) * sizeof(uint8_t));
+  memset(gshare_global_predictor.state, 0, (GSHARE_MAX_BIT) * sizeof(uint8_t));
+
+  // 2. Initialize TOURNAMENT-based predictor
+
+  // 3. Initialize CUSTOM predictor
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -62,16 +78,32 @@ make_prediction(uint32_t pc)
   //
   //TODO: Implement prediction scheme
   //
+  if (bpType == GSHARE)
+  {
+    int index = 0;
+    for (int i = 0; i < gshare_global_predictor.bit; i++)
+      index += gshare_global_predictor.pattern[i] * (1 >> i);
+
+    gshare_global_predictor.last_pred_index = index;
+    return gshare_global_predictor.state[index];
+  }
+  else if (bpType == TOURNAMENT)
+  {
+  }
+  else if (bpType == CUSTOM)
+  {
+  }
 
   // Make a prediction based on the bpType
-  switch (bpType) {
-    case STATIC:
-      return TAKEN;
-    case GSHARE:
-    case TOURNAMENT:
-    case CUSTOM:
-    default:
-      break;
+  switch (bpType)
+  {
+  case STATIC:
+    return TAKEN;
+  case GSHARE:
+  case TOURNAMENT:
+  case CUSTOM:
+  default:
+    break;
   }
 
   // If there is not a compatable bpType then return NOTTAKEN
@@ -82,10 +114,21 @@ make_prediction(uint32_t pc)
 // outcome 'outcome' (true indicates that the branch was taken, false
 // indicates that the branch was not taken)
 //
-void
-train_predictor(uint32_t pc, uint8_t outcome)
+void train_predictor(uint32_t pc, uint8_t outcome)
 {
   //
   //TODO: Implement Predictor training
   //
+  // 1. Train GSHARE predictor
+  for (int i = 0; i < gshare_global_predictor.bit - 1; i++)
+  {
+    gshare_global_predictor.pattern[i] = gshare_global_predictor.pattern[i + 1];
+  }
+  gshare_global_predictor.pattern[gshare_global_predictor.bit - 1] = outcome;
+
+  int last_prediction = gshare_global_predictor.state[gshare_global_predictor.last_pred_index];
+  if (last_prediction != outcome)
+    gshare_global_predictor.state[gshare_global_predictor.last_pred_index] = 1 - last_prediction;
+
+  // 2. Train TOURNAMENT predictor
 }
